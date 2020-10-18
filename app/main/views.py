089a0b8b2055e3b7f738bd653821ea17eb2c45c5
SharonFaith <sharonfaith15@gmail.com>
2020-10-18 15:59:15 +0300

    define index function in views file to display news sources

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 #from app import app
 from . import main
+from ..request import get_news_sources
 
 #Views
 @main.route('/')
@@ -9,4 +10,9 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    return render_template('index.html')
+    news_sources = get_news_sources()
+    print(news_sources)
+
+    title = 'Home-Welcome to the best app to get information on current affairs'
+
+    return render_template('index.html', title = title, sources = news_sources)
