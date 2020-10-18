4885fac83a84eeb91989230cbd74eec3ece09b13
SharonFaith <sharonfaith15@gmail.com>
2020-10-18 15:54:54 +0300

    define news_article class

--- app/models.py
+++ app/models.py
@@ -10,3 +10,15 @@ class News_Source:
         self.url = url
         self.category = category
 
+class News_Article:
+    '''
+    To define news article objects
+    '''
+    
+    def __init__(self, source_id, image_url, title, description, time, url):
+        self.id = source_id
+        self.image_url = image_url
+        self.title = title
+        self.description = description
+        self.time = time
+        self.url = url
