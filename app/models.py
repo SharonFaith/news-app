ce056d407d26e48791c5e52c4ffb209f6f541b3d
SharonFaith <sharonfaith15@gmail.com>
2020-10-18 15:53:10 +0300

    define news_source class

---
+++ app/models.py
@@ -0,0 +1,12 @@
+class News_Source:
+    '''
+    To define news source objects
+    '''
+    
+    def __init__(self, id, name, description, url, category):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+
